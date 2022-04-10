ff5e9ed3d075182b78576aaaf8afa49ba57597c3
Simon Voglimacci <s.voglimaccI@tuta.io>
2022-04-09 20:36:14 -0400

    fix: part 0-5
    feat: exercises 7.9.-7.10.

---
+++ part7/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,79 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
+})
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+    const user = request.user
+
+    if (!user) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    if (!body.likes) {
+        body.likes = 0
+    }
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+        return response.status(204).end()
+    }
+
+    const user = request.user
+    if (user._id.toString() === blog.user._id.toString()) {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } else {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        blog,
+        { new: true }
+    )
+    response.json(updatedBlog)
+})
+
+module.exports = blogsRouter
